97a81bdf40fe031266705f61cdb318dffff629a4
ISHAANRIJAL <127655785+ISHAANRIJAL@users.noreply.github.com>
2024-02-24 11:47:30 +0545

    urls

--- backend_api/main/urls.py
+++ backend_api/main/urls.py
@@ -6,22 +6,23 @@ router = routers.DefaultRouter()
 router.register('address', views.CustomerAddressViewSet)
 router.register('productrating', views.ProductRatingViewSet)
 
-
-
-urlpatterns = [
-    ##VENDORS
+urlpatterns=[
+    #VENDORS
     path('vendors/', views.VendorList.as_view()),
     path('vendors/<int:pk>/', views.VendorDetail.as_view()),
-    ##PRODUCTS
-    path('products/', views.ProductList.as_view()),
+
+    #Products
+    path('products/', views.VendorDetail.as_view()),
     path('products/<int:pk>/', views.ProductDetail.as_view()),
-    ##CUSTOMERS
+
+    #Customers
     path('customers/', views.CustomerList.as_view()),
     path('customers/<int:pk>/', views.CustomerDetail.as_view()),
-    #ORDER
+
+    #Orders
     path('orders/', views.OrderList.as_view()),
     path('orders/<int:pk>', views.OrderDetail.as_view()),
-
 ]
+
 urlpatterns = router.urls
 
